d27f88559f0c8de98ab783b3e7e2c85964b34e8b
natavunjian <natalie.avunjian@SADA-HQ-LATLAP1.safoian.com>
2015-10-30 16:21:24 -0700

    Update write in product code

--- setOppProdPrice.js
+++ setOppProdPrice.js
@@ -3,17 +3,23 @@ function form_OnLoad() {
 }
 
 function set_price_from_quantity() {
-     //initialize / define variables
+
+     //initialize and define variables
      var quantity = SADA.Field("quantity");
      var quant_value = SADA.Field("quantity").GetValue();
      var product = SADA.LookupField("productid");
-     var price = SADA.Field("dfi_priceperunit");
+     var price = SADA.Field("dfi_priceperunit").GetValue;
      var final_price = SADA.Field("priceperunit");
+     var writein_product = Xrm.Page.getAttribute("isproductoverridden").getValue();
      var qty1_10 = SADA.Field("dfi_qty1_10").GetValue();
      var qty11_50 = SADA.Field("dfi_qty11_50").GetValue();
      var qty51_100 = SADA.Field("dfi_qty51_100").GetValue();
      var qty101 = SADA.Field("dfi_qty101").GetValue();
-     var writein_product = Xrm.Page.getAttribute("isproductoverridden").getValue();
+
+     if (price.GetValue() != null) {
+          final_price.SetValue(price.getValue());
+          Xrm.Page.data.entity.save();
+     }
 
      // if write in product is false (existing product)
      if (writein_product === false) {
@@ -27,14 +33,13 @@ function set_price_from_quantity() {
                else if (quant_value > 100)
                     setPrice(price, product, "dfi_qty101");
                else {
-                    alert("Can't have negative or zero quantity!");
                     quantity.Clear();
                }
      }
      else if (writein_product === true) {
           if (price.GetValue() != null)
           //set write in product price based on quantity entered 
-               if(quant_value > 0 && quant_value < 11)
+               if(quant_value >= 0 && quant_value < 11)
                     final_price.SetValue(qty1_10);
                else if(quant_value > 10 && quant_value < 51)
                     final_price.SetValue(qty11_50);
@@ -43,7 +48,6 @@ function set_price_from_quantity() {
                else if(quant_value > 100)
                     final_price.SetValue(qty101);
                else {
-                    alert("Can't have negative or zero quantity!");
                     quantity.Clear();
                }          
      }
@@ -66,5 +70,9 @@ function clearExistingFields () {
           SADA.Field("dfi_qty51_100").Hide();
           SADA.Field("dfi_qty101").Hide(); 
           SADA.Field("dfi_priceperunit").Hide();
+          SADA.Field("dfi_qty1_10profitmargin").Hide();
+          SADA.Field("dfi_qty11_50profitmargin").Hide();
+          SADA.Field("dfi_qty51_100profitmargin").Hide();
+          SADA.Field("dfi_qty101_profitmargin").Hide();
      }
 }
